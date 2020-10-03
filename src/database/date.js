897438074019319eea14070fc000db8b29636de6
sleonia <sleonia@student.21-school.ru>
2020-10-03 21:48:55 +0300

    Make recoursive bypass for json

--- src/database/date.js
+++ src/database/date.js
@@ -1,17 +1,34 @@
 const getMonthsName = (index) => {
 	const monthsNames = [
-		"January", "February", "March",
-		"April", "May", "June",
-		"July", "August", "September",
-		"October", "November", "December"
+		"january", "february", "march",
+		"april", "may", "june",
+		"july", "august", "september",
+		"october", "november", "december"
 	];
 	return monthsNames[index];
 };
 
-
 class Data { //rename class // async???
 	constructor() {
 		this.date = new Date();
+	}
+
+	setDayValueFromJson(year = 2020, month = getMonthsName(0), day = 1, json = {}, value = '') {
+		for (let i in json) {
+			if (json[i].hasOwnProperty('year') && json[i].year === 2020) {
+				this.setDayValueFromJson(year, month, day, json[i].months);
+			} else if (json[i].hasOwnProperty(month) && json[i].months === month) {
+				this.setDayValueFromJson(year, month, day, json[i][month]);
+			} else {
+				for (let k in json) {
+					if (json[k][month] && json[k][month][day - 1]) {
+						console.log(json[k][month][day - 1][day]);
+						json[k][month][day - 1][day] = value;
+					}
+				}
+			}
+		}
+		return true;
 	}
 	
 	addNewUser(userInfo) {
@@ -47,21 +64,41 @@ class Data { //rename class // async???
 		return user;
 	}
 
-	async addItem(userData, newMessage) {
-		let json = JSON.parse(userData);
-		const years = json.years[json.years.length - 1];
-		const months = years.months[years.months.length - 1];
-		const currentMonth = getMonthsName(this.date.getMonth());
-		const day = months[currentMonth][months[currentMonth].length - 1];
-		Object.values(day)[0] = newMessage;
-		return userData;
+	addItem(userData, newMessage) {
+		// let json = JSON.parse(userData);
+		// const yearsLen = json.years.length - 1;
+		// const monthsLen = json.years[yearsLen].months.length - 1;
+		// const currentMonth = getMonthsName(this.date.getMonth());
+		// const daysLen = json.years[yearsLen].months[monthsLen][currentMonth].length - 1;
+
+		// // console.log(JSON.stringify(json));
+
+		// this.setDayValueFromJson(2020, 'october', '1', json.years);
+
+		// return JSON.stringify(json);
 	}
 
-	async getFullDate() {
-		return await `${this.date.getDay()}/${this.date.getMonth()}/${this.date.getFullYear()}/`;
+	getFullDate() {
+		return `${this.date.getDay()}/${this.date.getMonth()}/${this.date.getFullYear()}/`;
 	}
 }
 
-const data = new Data();
-console.log(data.addNewUser({id: 0, username: 'EmbodimentEvil'}));
-console.log(data.addItem('{"years":[{"year":2020,"months":[{"October":[{"6":"456"}]}]}]}', '123'));
+let data = new Data();
+// console.log(data.addNewUser({id: 0, username: 'EmbodimentEvil'}));
+// console.log(JSON.parse(data.addItem(
+
+let json = JSON.parse(
+'{"years":[{"year":2020,"months":[{"september":[{"29":"Fish 250\\nSnacks 340\\nBread 500\\n"},{"30":"Milk shake 600\\nMeat and bread 400\\n"}]},{"october":[{"1":"Apple 100\\nIce-cream 800\\nBanana 200\\n"},{"2":"Milk 200\\nCookies 400\\nCoca cola 240\\n"},{"3":"Protein shake 500\\nCookies 400\\nChocolate 700\\n"}]}]}]}'
+).years;
+
+console.log(data.setDayValueFromJson(
+	2020, 'october', '2', json
+));
+
+for (let i in json) {
+	for (let j in json[i]) {
+		for (let k in json[i][j]) {
+			console.log(json[i][j][k]);
+		}
+	}
+}
